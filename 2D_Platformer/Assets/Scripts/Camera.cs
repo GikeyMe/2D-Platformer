e1c573e96f44646cad7f256126f6b3b2a3ee6798
Michael Gallagher <rfmikey@gmail.com>
2016-01-07 23:40:00 +0000

    Platform and Camera
    New small platform and medium platform, platform collision script to
    accompany it. Also new camera script so that it follows the player.

---
+++ 2D_Platformer/Assets/Scripts/Camera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    [SerializeField]
+    private float xLimit;
+    [SerializeField]
+    private float yLimit;
+
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float yMin;
+
+    private Transform PlayerPosition;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        PlayerPosition = GameObject.Find("Player").transform;
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = new Vector3(Mathf.Clamp(PlayerPosition.position.x, xMin, xLimit), Mathf.Clamp(PlayerPosition.position.y, yMin, yLimit), transform.position.z);
+	}
+}
---
+++ 2D_Platformer/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTrigger : MonoBehaviour {
+
+    private BoxCollider2D playerHitBox;
+    [SerializeField]
+    private BoxCollider2D platformCollider;
+    [SerializeField]
+    private BoxCollider2D platformTrigger;
+
+	// Use this for initialization
+	void Start () {
+        playerHitBox = GameObject.Find("Player").GetComponent<BoxCollider2D>();  // get the player character's hitbox
+        Physics2D.IgnoreCollision(platformCollider, platformTrigger, true);      //ignore collision between the two collision boxes on the platform
+	}
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Physics2D.IgnoreCollision(platformCollider, playerHitBox, true);   //player walked through trigger box so disable collision 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Physics2D.IgnoreCollision(platformCollider, playerHitBox, false);  //player left trigger box so enable collision again
+        }
+    }
+}
